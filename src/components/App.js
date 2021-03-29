60e8e16af263ebfb057f0ee75ea78ce815c6efcb
Ernesto Resende <ernestoresende@hotmail.com>
2021-03-29 03:57:52 -0300

    chore: added ThemeContext to app level component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { ThemeProvider } from './ThemeContext'
+
+function App({ children }) {
+  return (
+    <ThemeProvider>
+      {children}
+    </ThemeProvider> 
+  )
+}
+
+export default App
