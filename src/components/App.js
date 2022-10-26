a2f2523452f1d8b9e9cb6cafbb9b597ce3ac1791
Ernesto Resende <dev@ernestoresende.com>
2022-10-26 09:13:10 -0300

    repo reset

--- src/components/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import GlobalContextProvider from '../styles/globalContext'
-
-function App({ children }) {
-  return <GlobalContextProvider>{children}</GlobalContextProvider>
-}
-
-export default App
