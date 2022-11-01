f300dcb663e7c0161e9681b714e7658d6feafd79
Ernesto Resende <dev@ernestoresende.com>
2022-11-01 14:29:19 -0300

    updated tailwind config with theme extensions

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,25 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      zIndex: {
+        'background': '-5',
+        'behind': '-1',
+      },
+      fontFamily: {
+        'display': ['Nunito', 'ui-sans-serif', 'system-ui'],
+        'body': ['Inter', 'ui-sans-serif', 'system-ui']
+      },
+      colors: {
+        'eletric-pink': '#F42A8B',
+        'deep-lilac': '#9D4CCE',
+        'french-blue': '#1F89DB',
+        'chinese-black': '#13111C',
+        'flash-white': '#F1F1F2',
+        'lavander-gray': '#C4C1D4',
+        'light-slate': '#777F9B',
+      }
+    }
   },
   plugins: []
 }
