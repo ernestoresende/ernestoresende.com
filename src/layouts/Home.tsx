049d2f2ccebcd6ee176b969c1d175ca968a5fc21
Ernesto Resende <dev@ernestoresende.com>
2022-12-22 02:00:26 -0300

    chore: updated layout component

--- src/layouts/Home.tsx
+++ src/layouts/Home.tsx
@@ -16,13 +16,16 @@ export const HomeLayout: React.FC<HomeLayoutProps> = ({
     <>
       <NavHeader />
       <Image
-        className="-z-10 absolute top-0"
+        className="absolute top-0 -z-10"
         src={BackgroundGradient}
         fill={true}
         quality={50}
         alt=""
       />
       <main className="px-8">{children}</main>
+      <div className="-z-10 relative h-36 w-full">
+        <div className="absolute top-0 h-80 w-full bg-gradient-to-t from-eletric-pink/10" />
+      </div>
       <NavFooter withWavyBackground={withWavyBackground} />
     </>
   )
