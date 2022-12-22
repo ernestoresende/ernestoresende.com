39d8f985f34a264741bc39c5c9a5922edb456338
Ernesto Resende <dev@ernestoresende.com>
2022-12-22 02:01:34 -0300

    feat: updated now page

--- pages/now.tsx
+++ pages/now.tsx
@@ -1,16 +1,23 @@
 import { NextPage } from 'next'
+import Link from 'next/link'
 import { TitleAndMetaTags } from '@/components/Seo'
 import { HomeLayout } from '@/layouts/Home'
+import { IoChevronBack } from 'react-icons/io5'
 
 const NowPage: NextPage = () => {
   return (
     <>
-      <TitleAndMetaTags
-        title="what I'm doing now - Ernesto Resende"
-      />
+      <TitleAndMetaTags title="what I'm doing now - Ernesto Resende" />
       <HomeLayout>
         <section className="pt-32 pb-20 h-full flex justify-center items-center">
           <div className="max-w-3xl w-full">
+            <Link
+              href="/"
+              className="inline-flex items-center justify-center rounded-md border border-transparent bg-transparent mb-12 text-center font-semibold text-sm text-flash-white transition duration-100"
+            >
+              <IoChevronBack className="h-4 w-4" />
+              Go Back
+            </Link>
             <h1 className="font-display font-bold text-4xl pb-2">
               what I&apos;m doing now.
             </h1>
@@ -24,9 +31,7 @@ const NowPage: NextPage = () => {
               <li className="pb-4">
                 Studying some library level TypeScript wizardry
               </li>
-              <li className="pb-4">
-                Picking up some Rust knowledge
-              </li>
+              <li className="pb-4">Picking up some Rust knowledge</li>
               <li className="pb-4">
                 Playing way too much No Man&apos;s Sky (space exploration is
                 fun)
