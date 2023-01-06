2a4417f5599fa7ce6e39da49119d94c2d1853220
Ernesto Resende <dev@ernestoresende.com>
2023-01-06 22:02:15 +0000

    chore: added aria-label to return link on header logo

--- src/components/NavHeader.tsx
+++ src/components/NavHeader.tsx
@@ -17,23 +17,23 @@ export const NavHeader: React.FC = () => {
       })}
     >
       {/* Backdrop blur layer */}
-      <div className="relative flex items-center justify-center px-8 h-14 backdrop-blur-xl">
+      <div className="relative flex h-14 items-center justify-center px-8 backdrop-blur-xl">
         {/* Container layer for the navigation */}
-        <div className="max-w-3xl w-full h-full border-light-slate/10 border-b flex justify-between">
+        <div className="flex h-full w-full max-w-3xl justify-between border-b border-light-slate/10">
           {/* Left align portion of the NavBar */}
           <div className="flex items-center justify-center gap-16">
-            <Link href="/">
+            <Link aria-label="Return to main page" href="/">
               <MainLogo className="h-4" />
             </Link>
             {/* Navigation links */}
-            <ul className="flex gap-8 text-lavander-gray text-sm font-body font-semibold">
-              <li className="hover:text-flash-white transition duration-300">
+            <ul className="flex gap-8 font-body text-sm font-semibold text-lavander-gray">
+              <li className="transition duration-300 hover:text-flash-white">
                 <a href="#about">me</a>
               </li>
-              <li className="hover:text-flash-white transition duration-300">
+              <li className="transition duration-300 hover:text-flash-white">
                 <a href="#work">work</a>
               </li>
-              <li className="hover:text-flash-white transition duration-300">
+              <li className="transition duration-300 hover:text-flash-white">
                 <a href="#contact">contact</a>
               </li>
             </ul>
